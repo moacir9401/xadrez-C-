5c357095fcbebd8dc43af5da27900aeb1fd83b6f
Moacir <moacir9401@gmail.com>
2021-01-02 16:12:12 -0300

    criando a class posicaoXadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -10,13 +10,17 @@ namespace Xadrez
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                //Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-                Tela.imrpimirTabuleiro(tab);
+                //Tela.imrpimirTabuleiro(tab);
+
+
+                var pos = new PosicaoXadrez('c',7);
+                Console.WriteLine(pos.toPosicao());
             }
             catch (TabuleiroExceptions e)
             {
