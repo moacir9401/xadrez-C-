ae5e50106a0102c60f253edb5578fbf587dec8bb
Moacir <moacir9401@gmail.com>
2021-01-02 12:38:19 -0300

    Começando pecas no tabuleiro

--- Xadrez/tabuleiro/Peca.cs
+++ Xadrez/tabuleiro/Peca.cs
@@ -9,9 +9,9 @@
         public Tabuleiro tab { get; set; }
          
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca( Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tab = tab;
             this.cor = cor;
             qtdMovimentos = 0;
